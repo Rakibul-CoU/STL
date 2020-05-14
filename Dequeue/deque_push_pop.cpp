d7d38c49d17ceae97d0a47d8eb2be6cac3d4f804
Md. Rakibul Islam <39064963+Rakibul-CoU@users.noreply.github.com>
2020-05-14 13:32:45 +0600

    Add files via upload

---
+++ Dequeue/deque_push_pop.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+#define PI 2.0*acos(0.0)
+#define ll              long long int
+#define gcd(a,b)        __gcd(a,b)
+#define lcm(a,b)        ((a)*(b))/gcd(a,b)
+#define MOD             1000000007
+#define mx              123456789
+using namespace std;
+int main()
+{
+    deque<int>dq;
+    int n, m, i, j, k, l;
+    dq.push_front(5);
+    dq.push_front(6);
+    for(i=0; i<dq.size(); i++)
+    {
+        cout<<dq[i]<<" ";
+    }
+    cout<<endl;
+    dq.pop_front();
+    for(i=0; i<dq.size(); i++)
+    {
+        cout<<dq[i]<<" ";
+    }
+    cout<<endl;
+    dq.push_back(9);
+    dq.push_back(10);
+    for(i=0; i<dq.size(); i++)
+    {
+        cout<<dq[i]<<" ";
+    }
+    cout<<endl;
+    dq.pop_back();
+    for(i=0; i<dq.size(); i++)
+    {
+        cout<<dq[i]<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
---
+++ Dequeue/deque_size_erase.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+#define PI 2.0*acos(0.0)
+#define ll              long long int
+#define gcd(a,b)        __gcd(a,b)
+#define lcm(a,b)        ((a)*(b))/gcd(a,b)
+#define MOD             1000000007
+#define mx              123456789
+using namespace std;
+int main()
+{
+    deque<int>dq;
+    int n, m, i, j, k, l;
+    dq.push_back(5);
+    dq.push_back(2);
+    dq.push_back(10);
+    dq.push_back(7);
+    dq.push_back(4);
+    for(i=0; i<dq.size(); i++)
+    {
+        if(dq[i]%2==0)
+        {
+            dq.erase(dq.begin()+i);
+        }
+    }
+    for(i=0; i<dq.size(); i++)
+    {
+        cout<<dq[i]<<" ";
+    }
+    cout<<endl;
+    dq.clear();
+    cout<<dq.size()<<endl;
+    return 0;
+}
