7840e397c0df7bf700bdfcdbac7153d8f4db3c0b
Md. Rakibul Islam <39064963+Rakibul-CoU@users.noreply.github.com>
2020-05-14 13:35:19 +0600

    Add files via upload

---
+++ Stack/stack_empty.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#define PI 2.0*acos(0.0)
+#define ll              long long int
+#define gcd(a,b)        __gcd(a,b)
+#define lcm(a,b)        ((a)*(b))/gcd(a,b)
+#define MOD             1000000007
+#define mx              123456789
+using namespace std;
+int main()
+{
+    stack<int>s;
+    int n, m, i, j, k, l;
+    cin>>n;
+    for(i=0; i<n; i++)
+    {
+        int x;
+        cin>>x;
+        s.push(x);
+    }
+    while(!s.empty())
+        {
+            cout<<s.top()<<endl;
+            s.pop();
+        }
+    //s.push(5);
+    cout<<"Is stack empty: "<<s.empty()<<endl;
+    return 0;
+}
---
+++ Stack/stack_push_pop_top_size.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+#define PI 2.0*acos(0.0)
+#define ll              long long int
+#define gcd(a,b)        __gcd(a,b)
+#define lcm(a,b)        ((a)*(b))/gcd(a,b)
+#define MOD             1000000007
+#define mx              123456789
+using namespace std;
+int main()
+{
+    stack<int>s;
+    int n, m, i, j, k, l;
+    cin>>n;
+    for(i=0; i<n; i++)
+    {
+        int x;
+        cin>>x;
+        s.push(x);
+    }
+    cout<<s.size()<<endl;
+
+    cout<<"Top element: ";
+    cout<<s.top()<<endl;
+
+    s.pop();
+    cout<<"After pop top element: ";
+    cout<<s.top()<<endl;
+    return 0;
+}
