2087c17e8413ca5d9a1265be86864e2ed9232248
Md. Rakibul Islam <39064963+Rakibul-CoU@users.noreply.github.com>
2020-05-14 13:30:57 +0600

    Add files via upload

---
+++ Queue/queue_empty_element_access.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#define PI 2.0*acos(0.0)
+#define ll              long long int
+#define gcd(a,b)        __gcd(a,b)
+#define lcm(a,b)        ((a)*(b))/gcd(a,b)
+#define MOD             1000000007
+#define mx              123456789
+using namespace std;
+int main()
+{
+    queue<int>q;
+    int n, m, i, j, k, l;
+    cin>>n;
+    for(i=0; i<n; i++)
+    {
+        int x;
+        cin>>x;
+        q.push(x);
+    }
+    while(!q.empty())
+          {
+             cout<<q.front()<<endl;
+                q.pop();
+          }
+    cout<<"Is queue empty: "<<q.empty()<<endl;
+    cout<<q.size()<<endl;
+    return 0;
+}
